6b189ddfe4b082a55915cc8248a8d8c4b0374b56
Abdulhamit4216 <abdulhamittiki44@gmail.com>
2025-02-19 12:17:13 +0300

    Anahtar labeli oluşturuldu.

--- CaesarPassword/src/main/java/org/example/Password.java
+++ CaesarPassword/src/main/java/org/example/Password.java
@@ -6,10 +6,14 @@ import java.awt.*;
 public class Password {
     JFrame frame;
 
+    JLabel keyLabel;
+
     Password(){
         frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new GridLayout(5,2));
+
+        keyLabel = new JLabel("Anahtar: ");
 
     }
 }
