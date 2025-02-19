e2395eea5a41077323edad8593f50a286d417ddd
Abdulhamit4216 <abdulhamittiki44@gmail.com>
2025-02-19 12:14:58 +0300

    Frame oluşturuldu.

--- CaesarPassword/src/main/java/org/example/Password.java
+++ CaesarPassword/src/main/java/org/example/Password.java
@@ -1,5 +1,15 @@
 package org.example;
 
-public class Password {
+import javax.swing.*;
+import java.awt.*;
 
+public class Password {
+    JFrame frame;
+
+    Password(){
+        frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new GridLayout(5,2));
+
+    }
 }
